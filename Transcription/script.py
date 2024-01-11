71dc604be1f2113c857bf38caee6008f82e0199e
AynurV <acauntd721@gmail.com>
2024-01-11 19:26:58 +0500

    Добавьте файлы проекта.

---
+++ Transcription/script.py
@@ -0,0 +1,15 @@
+﻿from pyannote.audio import Pipeline
+pipeline = Pipeline.from_pretrained(
+    "pyannote/speaker-diarization-3.1",
+    use_auth_token="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")
+
+# send pipeline to GPU (when available)
+import torch
+pipeline.to(torch.device("cpu"))
+
+# apply pretrained pipeline
+diarization = pipeline(r"C:\Users\AynurV\Downloads\zapis300.mp3")
+
+# print the result
+for turn, _, speaker in diarization.itertracks(yield_label=True):
+    print(f"[<start={turn.start:.1f} stop={turn.end:.1f} speaker={speaker}>]")
